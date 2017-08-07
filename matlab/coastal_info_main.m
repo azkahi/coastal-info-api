6433671f9fea982409e0b0cf107c4303828b801a
azkahi <you@example.com>
2017-08-08 01:17:47 +0700

    Fixed naming

--- matlab/coastal_info_main.m
+++ matlab/coastal_info_main.m
@@ -1,3 +1,3 @@
 timeStart = datenum(2014,10,8,0,0,0);
 timeEnd = datenum(2017,5,17,0,0,0);
-Program_Prediksi2015('Data.txt', 'Complete.txt', 'Predict.txt', -8.21, 1.61, timeStart, timeEnd);
+Program_Prediksi2015('Data.txt', 'Complete.txt', 'Predict.txt', -8.19, 1.48, timeStart, timeEnd);
